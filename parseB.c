89b1e30be987e7b0cf071b8cc0bb87e0f724bf7e
johncoleman83 <lcsw@davidjohncoleman.com>
2017-04-17 20:01:22 +0000

    shortened variables

--- parseB.c
+++ parseB.c
@@ -10,34 +10,45 @@ void expand_bash_vars(arg_inventory_t *arginv)
 {
 	unsigned int i, j;
 	env_t *node;
+	tokens_t t = arginv->tokens;
 
-	for (i = 0; i < arginv->tokens.tokensN; i++)
-		if (arginv->tokens.tokens[i].id == TOKEN_STRING)
+	for (i = 0; i < t.tokensN; i++)
+		if (t.tokens[i].id == TOKEN_STRING)
 		{
-			for (j = 0; j < _strlen(arginv->tokens.tokens[i].str); j++)
-				if (arginv->tokens.tokens[i].str[j] == '$')
-					switch (arginv->tokens.tokens[i].str[j + 1])
+			for (j = 0; j < _strlen(t.tokens[i].str); j++)
+				if (t.tokens[i].str[j] == '$')
+					switch (t.tokens[i].str[j + 1])
 					{
 						case '$':
-							replace_str((char **)&arginv->tokens.tokens[i].str, int_to_str(getpid()), j, j + 1, 1);
+							replace_str((char **)&t.tokens[i].str,
+										int_to_str(getpid()), j, j + 1, 1);
 							break;
 						case '?':
-							replace_str((char **)&arginv->tokens.tokens[i].str, int_to_str(arginv->last_exit_code), j, j + 1, 1);
+							replace_str((char **)&t.tokens[i].str,
+										int_to_str(arginv->last_exit_code), j,
+										j + 1, 1);
 							break;
 						case '!':
-							replace_str((char **)&arginv->tokens.tokens[i].str, (arginv->last_bg_pid == -1) ? "" : int_to_str(arginv->last_bg_pid), j, j + 1, 1);
+							replace_str((char **)&t.tokens[i].str,
+										(arginv->last_bg_pid == -1) ? "" :
+										int_to_str(arginv->last_bg_pid), j,
+										j + 1, 1);
 							break;
 						case '0':
-							replace_str((char **)&arginv->tokens.tokens[i].str, "hsh", j, j + 1, 0);
+							replace_str((char **)&t.tokens[i].str, "hsh", j,
+										j + 1, 0);
 							break;
 						default:
-							node = fetch_node(arginv->envlist, (char *)&arginv->tokens.tokens[i].str[j + 1]);
-							replace_str((char **)&arginv->tokens.tokens[i].str, (node == NULL) ? "" : node->val, j, _strlen(arginv->tokens.tokens[i].str) - 1, 0);
+							node = fetch_node(arginv->envlist,
+											  (char *)&t.tokens[i].str[j + 1]);
+							replace_str((char **)&t.tokens[i].str,
+										(node == NULL) ? "" : node->val, j,
+										_strlen(t.tokens[i].str) - 1, 0);
 					}
-				else if (arginv->tokens.tokens[i].str[j] == '~' && j == 0)
+				else if (t.tokens[i].str[j] == '~' && j == 0)
 				{
 					node = fetch_node(arginv->envlist, "HOME");
-					replace_str((char **)&arginv->tokens.tokens[i].str, node->val, j, j, 0);
+					replace_str((char **)&t.tokens[i].str, node->val, j, j, 0);
 				}
 		}
 }
@@ -69,7 +80,8 @@ void expand_alias(arg_inventory_t *arginv)
 		commands = safe_malloc((count + cmd_tokens.tokensN) * sizeof(char *));
 		for (i = count - 1; i >= 1; i--)
 		{
-			commands[i + cmd_tokens.tokensN - 1] = (char *)_strdup((char *)arginv->commands[i]);
+			commands[i + cmd_tokens.tokensN - 1] =
+				(char *)_strdup((char *)arginv->commands[i]);
 		}
 
 		for (i = 0; i < cmd_tokens.tokensN; i++)
